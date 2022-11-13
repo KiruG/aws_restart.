e51024ba5d54205014204fa3f431be36f361ea58
KiruG <58433504+KiruG@users.noreply.github.com>
2022-11-13 13:17:22 +0300

    Add files via upload
    AWS re/start python labs

---
+++ Untitled1.py
@@ -0,0 +1,5 @@
+hello="hey"
+
+name="kirubel"
+nume="asidad"
+print("{}  says {} sds {}".format(name,hello,nume))
---
+++ while-loop.py
@@ -0,0 +1,28 @@
+""" 
+import random
+
+print("Welcome to Guess the Number!")
+print("The rules are simple. I will think of a number, and you will try to guess it.")
+
+number = random.randint(1,10)
+isGuessRight = False
+
+# while loop to guess the number 
+while isGuessRight != True:
+    guess = input("Guess a number between 1 and 10: ")
+    if int(guess) == number:
+        print("You guessed {}. That is correct! You win!".format(guess))
+        isGuessRight = True
+    else:
+        print("You guessed {}. Sorry, that isn’t it. Try again.".format(guess))
+        
+        
+
+this code asks users to guess a number 
+between 1 and 10
+"""
+ 
+print("count to 10")
+for x in range (1,11):
+    print(x)
+ 
